00944a3fa1183e94247abb354ca68af5b003e512
Noura Loudani <contact.nouraloudani@gmail.com>
2020-11-08 21:05:03 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -10,7 +10,8 @@ def second_challenge
         :patriarch => {},
         :matriarch => {},
         :heroine => {},
-       :heroine_friends => []
+        :heroine_friends => []
       }
   }
+  
 end
