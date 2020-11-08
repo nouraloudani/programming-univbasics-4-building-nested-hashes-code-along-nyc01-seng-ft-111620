17ca753007df6fc2cfbbecb78e76497ac194a74e
Noura Loudani <contact.nouraloudani@gmail.com>
2020-11-08 20:58:50 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,13 +1,9 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},        },
-      :matriarch => {
-        #your key/value pairs here
-        },
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {
-        #your key/value pairs here
-        },
       :hero_friends => []
    }, 
    :capulet => {
